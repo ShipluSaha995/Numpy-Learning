bf70b2677060c5432b2eb93de65b1481ff5af887
ShipluSaha995 <shiplusaha995@gmail.com>
2026-01-12 18:41:22 +0600

    updated numpy1 file

---
+++ Numpy1.py
@@ -0,0 +1,16 @@
+# Numpy is the fundamental packedge or core python library for scientific computing or numerical computing in python.
+# it is much faster and more memory efficient.
+
+'''
+Numpy Array: A numpy array is a grid of values and it contains information about the raw data,
+how to locate an element and how to interpret an element.
+
+We can also say that it is a collection of the elements of same data-type.
+
+key point:
+
+--> Stored in conteguous memory  
+--> Can be 1 dimensional, 2 dimensional and multi-dimensional  
+--> Optimized for fast mathematical operations  
+
+'''  
