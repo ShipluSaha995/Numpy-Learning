02ef31f781740080e4bba546060a1022267ff60c
shiplusaha995 <shiplusaha995@gmail.com>
2026-05-26 20:44:14 +0600

    updated

---
+++ numpy _array.py
@@ -0,0 +1,13 @@
+# creating numpy array
+
+import numpy as np
+a=np.array([1,2,3])
+print(a)
+print(type(a))
+
+#2d and 3d numpy array
+b=np.array([[1,2,3],[3,4,5]])
+print("2d array: \n",b)
+
+c=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print("3d array: \n", c)
