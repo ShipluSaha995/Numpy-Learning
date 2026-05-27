c851f03a9bdbb1e76fe4a3f8239e0194fb283918
shiplusaha995 <shiplusaha995@gmail.com>
2026-05-27 21:03:02 +0600

    updated

--- numpy _array.py
+++ numpy _array.py
@@ -41,3 +41,30 @@ print("\narray: ", ee)
 
 f=np.arange(1,11).reshape(5,2)
 print("\n",f)
+
+
+
+# np.ones and np.zeros using this we can create an array on the go and all elements of this array will be one
+
+g=np.ones((3,4))
+print("\n",g)
+
+h=np.zeros((3,4))
+print("\n",h)
+
+
+# to initialize with random numbers ( it will give random numbers from 0 to 1 )
+
+i=np.random.random((3,4))
+print("\n",i)
+
+
+# np.linespace, suppose i want 10 random numbes from -10 to 10
+j=np.linspace(-10,10,10)
+print("\n",j)
+
+
+
+#np.identity used to create identity matrix
+k=np.identity(3)
+print("\n",k)
