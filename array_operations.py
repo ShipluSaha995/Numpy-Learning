a7954ace0034ec92f401175ba2b2c2bd012a026d
shiplusaha995 <shiplusaha995@gmail.com>
2026-05-29 21:09:49 +0600

    array_operations

---
+++ array_operations.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+a1=np.arange(12).reshape(3,4)
+a2=np.arange(12,24).reshape(3,4)
+
+#scaler operations
+#arithmetic operation
+
+print(a1*2)
+print(a2+2)
+print(a1-2)
+print(a2/2)
+print(a1**2)
+
+
+#relational
+
+print(a2<15)
+print(a2>15)
+
+
+#vector operator , when we apply a operation in two numpy array
+# arithmatic operation
+
+print("array: ", a1+a2)
+print("array: ", a1-a2)
+print("array: ", a1*a2)
+print("array: ", a1/a2)
+print("array: ", a1**a2)
