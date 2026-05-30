7cbc6720ab64114f662deb1fee5320a4ee7ea397
shiplusaha995 <shiplusaha995@gmail.com>
2026-05-30 17:53:03 +0600

    array_functions

--- Indexing_and_slicing.py
+++ Indexing_and_slicing.py
@@ -18,3 +18,19 @@ print(a2[1,2])
 print(a2[2,3])
 print(a2[2,1])
 print(a2[0,0])
+
+# 3D array
+
+print(a3)
+
+print(a3[0,1,1])
+print(a3[1,0,0])
+print(a3[1,1,0])
+
+
+# slicing
+print(a1)
+print(a1[2:5])
+print(a1[0:5])
+
+print(a1[2:5:2])
