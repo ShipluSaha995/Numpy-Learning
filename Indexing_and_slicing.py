6e1f96432704f5b7f1eab0cfd27c341278d224b8
shiplusaha995 <shiplusaha995@gmail.com>
2026-05-31 12:56:25 +0600

    3D array

--- Indexing_and_slicing.py
+++ Indexing_and_slicing.py
@@ -34,3 +34,40 @@ print(a1[2:5])
 print(a1[0:5])
 
 print(a1[2:5:2])
+# start = 2 → start from index 2
+# stop = 5 → go up to (but not including) index 5
+# step = 2 → move 2 positions at a time
+print(a1[1:9:2])
+print(a1[3:10:3]) 
+
+
+
+
+# 2D Array
+
+print(a2)
+print(a2[0,:])
+print(a2[:,2])
+print(a2[1:, 1:3])
+print(a2[1:2, 0:2])
+print(a2[2:, 2:4])
+print(a2[2:, 1:3])
+print(a2[0:, 2:4])
+print(a2[::2, ::3])
+print(a2[::2, 1::2])
+print(a2[1::2, ::3])
+print(a2[:2, 1:])
+
+
+#3D array
+
+a3=np.arange(27).reshape(3,3,3)
+print(a3)
+
+
+print(a3[1])
+print(a3[::2])
+print(a3[0,1,:])
+print(a3[1,:,1])
+print(a3[2,1:,1:])
+print(a3[::2, 0, ::2])
